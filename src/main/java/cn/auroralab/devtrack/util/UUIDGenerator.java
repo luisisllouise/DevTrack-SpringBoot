f2010c16b16d8bc1580d8f5a4144579a556ab705
阿夢 <ikaroinory233@gmail.com>
2022-10-16 00:12:41 +0800

    ci: Add bytesToHexString() function

--- src/main/java/cn/auroralab/devtrack/util/UUIDGenerator.java
+++ src/main/java/cn/auroralab/devtrack/util/UUIDGenerator.java
@@ -4,14 +4,6 @@ import java.util.UUID;
 
 public class UUIDGenerator {
     public static byte[] getUUID() {
-        String uuid = UUID.randomUUID().toString().replaceAll("-", "").toLowerCase();
-        int len = uuid.length() / 2;
-        char[] uuidChars = uuid.toCharArray();
-        byte[] bytes = new byte[len];
-        for (int i = 0; i < len; i++) {
-            int pos = i * 2;
-            bytes[i] = (byte) (ConvertTool.charToByte(uuidChars[pos]) << 4 | ConvertTool.charToByte(uuidChars[pos + 1]));
-        }
-        return bytes;
+        return ConvertTool.hexStringToBytes(UUID.randomUUID().toString().replaceAll("-", ""));
     }
 }
