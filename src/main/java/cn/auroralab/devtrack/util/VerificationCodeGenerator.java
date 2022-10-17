cb43baf589afd4e114021e7b54b2ed82a0d81243
阿夢 <ikaroinory233@gmail.com>
2022-10-17 12:18:08 +0800

    fix: verification code length

--- src/main/java/cn/auroralab/devtrack/util/VerificationCodeGenerator.java
+++ src/main/java/cn/auroralab/devtrack/util/VerificationCodeGenerator.java
@@ -8,23 +8,27 @@ import java.time.LocalDateTime;
 import java.util.Random;
 
 public class VerificationCodeGenerator {
+    private static final int DEFAULT_VERIFICATION_CODE_LENGTH = 6;
     /**
      * 验证码的默认有效时间，单位：分钟。
      */
     public static final int DEFAULT_VALID_TIME = 5;
 
     @Getter
-    private final String verificationCode;
-    @Getter
     private final LocalDateTime startTime;
+    @Getter
+    private final String verificationCode;
     private final int validTime;
 
     public VerificationCodeGenerator() { this(DEFAULT_VALID_TIME); }
 
     public VerificationCodeGenerator(int validTime) {
         Random random = new SecureRandom();
-        this.verificationCode = String.valueOf(random.nextInt(1000000));
         this.startTime = LocalDateTime.now();
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < DEFAULT_VERIFICATION_CODE_LENGTH; i++)
+            stringBuilder.append(random.nextInt(10));
+        this.verificationCode = stringBuilder.toString();
         this.validTime = validTime;
     }
 
